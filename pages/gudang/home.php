389d618354ae198e9f082017e9a9118a36b75c00
mariati14 <59953991+mariati14@users.noreply.github.com>
2020-01-20 15:13:57 +0700

    Add files via upload

---
+++ pages/gudang/home.php
@@ -0,0 +1,47 @@
+<div class="row-fluid">
+<ul class="thumbnails">
+<li class="span4">
+<div class="thumbnail">
+                  
+                  <div class="caption">
+                    <h3>Material</h3>
+                    <p>Halaman untuk menambah,mengubah dan menghapus data material</p>
+                    <p><a href="?cat=gudang&page=barang" class="btn btn-primary">Masuk</a> </p>
+                  </div>
+                </div>
+</li>
+
+
+<li class="span4">
+<div class="thumbnail">
+                  
+                  <div class="caption">
+                    <h3>Penerimaan Material</h3>
+                    <p>Menambahkan material ataupun menerima material untuk ditempatkan pada Gudang</p>
+                    <p><a href="?cat=gudang&page=entry" class="btn btn-primary">Masuk</a> </p>
+                  </div>
+                </div>
+</li>
+
+<li class="span4">
+<div class="thumbnail">
+                  
+                  <div class="caption">
+                    <h3> Material Keluar</h3>
+                    <p>Mengurangi material ataupun mengeluarkan material untuk dipakai </p>
+                    <p><a href="?cat=gudang&page=sell" class="btn btn-primary">Masuk</a> </p>
+                  </div>
+                </div>
+</li>
+<li class="span4">
+<div class="thumbnail">
+                  
+                  <div class="caption">
+                    <h3>Laporan Bulanan</h3>
+                    <p>Laporan bulanan untuk stok Gudang </p>
+                    <p><a href="?cat=gudang&page=monthreporting" class="btn btn-primary">Masuk</a> </p>
+                  </div>
+                </div>
+</li>
+</ul>
+</div>
---
+++ pages/pimpinan/home.php
@@ -0,0 +1,16 @@
+<div class="row-fluid">
+<ul class="thumbnails">
+<li class="span4">
+<div class="thumbnail">
+                  
+                  <div class="caption">
+                    <h3>Laporan Bulanan</h3>
+                    <p>Laporan bulanan untuk stok Gudang </p>
+                    <p><a href="?cat=pimpinan&page=monthreporting" class="btn btn-primary">Masuk</a> </p>
+                  </div>
+                </div>
+</li>
+
+
+</ul>
+</div>
